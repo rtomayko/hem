5bfe97cb6dfbc94026ba0a4dbe250fc0e49de461
Ryan Tomayko <rtomayko@gmail.com>
2008-03-07 01:24:21 -0500

    make dist and install work with man pages

--- hem.sh
+++ hem.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
-#
 # hem - ssh controller and automatic tunnel daemon.
 #
 # Copyright (c) 2008, Ryan Tomayko <r@tomayko.com>
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# See the file COPYING included with this package for license information.
 set -e
 
 USAGE="[-q][-v][-c <file>] <command> [<opt>]... [<profile>]...
